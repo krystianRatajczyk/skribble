e9b7bdecc1ce3054bc73cb927e7ba50812372cc8
krystianRatajczyk <ratajczyk.krystian7@gmail.com>
2024-03-17 14:57:04 +0100

    autoscroll added

--- client/components/chat/chat.tsx
+++ client/components/chat/chat.tsx
@@ -1,16 +1,30 @@
 "use client";
 
 import { useChat } from "@/hooks/use-chat-store";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import ChatItem from "./chat-item";
 
 const Chat = () => {
   const { messages } = useChat();
-  
+
+  const chatContainerRef = useRef<HTMLDivElement>(null);
+
+  const scrollToBottom = () => {
+    if (chatContainerRef.current) {
+      chatContainerRef.current.scrollTop =
+        chatContainerRef.current.scrollHeight;
+    }
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
   return (
     <div className="w-full flex flex-col items-center justify-center flex-1 overflow-auto mt-3">
       <p className="font-semibold text-xl">Chat</p>
       <div
+        ref={chatContainerRef}
         className="mt-3 dark:border-[#1e293b] border-[#dde9f9] border-[1px] rounded-md overflow-hidden overflow-y-auto no-scrollbar 
       h-full w-full dark:bg-[#050e1d] flex-1"
       >
