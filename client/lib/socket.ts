c5245eeef42b8428c542f78d6fabbd48c68c189c
krystianRatajczyk <ratajczyk.krystian7@gmail.com>
2024-03-18 19:52:08 +0100

    production link added

--- client/lib/socket.ts
+++ client/lib/socket.ts
@@ -2,6 +2,6 @@ import { io } from "socket.io-client";
 
 const URL =
   process.env.NODE_ENV === "production"
-    ? process?.env?.NEXT_PUBLIC_SITE_URL!
+    ? "https://skribble-server-blue.vercel.app"
     : "http://localhost:3001";
 export const socket = io(URL, { transports: ["websocket"] });
