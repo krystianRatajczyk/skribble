98aee8c482ec069d8ebf1efbef0a16ddca02d31b
krystianRatajczyk <ratajczyk.krystian7@gmail.com>
2024-03-19 19:36:04 +0100

    socket link changed

--- client/lib/socket.ts
+++ client/lib/socket.ts
@@ -2,6 +2,6 @@ import { io } from "socket.io-client";
 
 const URL =
   process.env.NODE_ENV === "production"
-    ? "https://skribble-server-blue.vercel.app"
+    ? "https://skribble-tcxp.vercel.app"
     : "http://localhost:3001";
 export const socket = io(URL, { transports: ["websocket"] });
